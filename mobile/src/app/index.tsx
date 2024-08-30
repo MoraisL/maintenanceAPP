90da3a2f49493e549ed28b4eb614d650594f6c8b
MoraisL <viniciusmoraaais@gmail.com>
2024-08-29 22:22:50 -0300

    feat: create components details and home

--- mobile/src/app/index.tsx
+++ mobile/src/app/index.tsx
@@ -1,15 +1,22 @@
-import { Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from '@react-navigation/stack';
+import { HomeScreen } from "../components/home";
+import { DetailsScreen } from "../components/details";
+
+type RootStackParamList = {
+  Home: undefined;
+  Details: {name: string; email: string};
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <NavigationContainer independent={true}>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
